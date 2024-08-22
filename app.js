87161032fc955e94b3fca107bf1f7e633bc7b377
PedroLucas-O <pedrolucas.pc.cp@gmail.com>
2024-08-22 15:44:50 -0300

    variavel

---
+++ app.js
@@ -0,0 +1,35 @@
+// let nome = prompt("Digite Seu nome:");
+// let corFav = prompt("Digite Sua Cor Favorita:");
+
+// console.log("Olá " +nome+ ", Sua cor favorita é: " +corFav+ "?")
+// console.log(`Seu nome é: ${nome.toUpperCase()}? \n E sua cor favorita é: ${corFav.toUpperCase()}!`)
+
+// console.log(`Seu nome é: ${nome}? \n E sua cor favorita é: ${corFav}!`)
+// console.log(nome.length, corFav.length)
+
+// console.log(nome.includes("a"))
+// console.log(corFav.includes("a"))
+
+// console.log(nome.replace("j", " Deu um presente para: "))
+
+// let nome = "Pedro Lucas Oliveira Frazão";
+// let idade = 11;
+// console.log(nome)
+// nome = "maria"
+// console.log(nome)
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+
+let a = 2;
+let b = 1;
+let c;
+console.log("A:" ,a);
+console.log("B:" ,b);
+console.log("C:" ,c);
+c = a;
+a = b;
+b = c;
+console.log("A:" ,a);
+console.log("B:" ,b);
+console.log("C:" ,c);
